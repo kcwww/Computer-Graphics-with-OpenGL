0696e044830c6fa90b80981face85966c92d3d0a
kcwww <cwkim0321@gmail.com>
2024-08-18 16:42:22 +0900

    feat: add window opengl

--- series02/src/main.cpp
+++ series02/src/main.cpp
@@ -1,6 +1,31 @@
 #include <spdlog/spdlog.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-    SPDLOG_INFO("Hello, World!!");
+    SPDLOG_INFO("Start Program");
+
+    //glfw 라이브러리 초기화
+    if (!glfwInit()) {
+        const char * description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_ERROR("Failed to initialize GLFW : {}", description);
+        return -1;
+    }
+
+    //glfw 윈도우 생성
+    SPDLOG_INFO("Create GLFW Window");
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+    if (!window) {
+        SPDLOG_ERROR("Failed to create GLFW window");
+        glfwTerminate();
+        return -1;
+    }
+
+    // glfw 루프
+    SPDLOG_INFO("Start GLFW Loop");
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+    glfwTerminate();
     return 0;
 }
