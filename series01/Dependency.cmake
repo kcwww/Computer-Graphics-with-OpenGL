51ac57cbf9542281b2623b791442b83561d93803
kcwww <cwkim0321@gmail.com>
2024-08-18 16:03:33 +0900

    feat: add glfw lib

--- series01/Dependency.cmake
+++ series01/Dependency.cmake
@@ -8,7 +8,7 @@ set(DEP_LIB_DIR ${DEP_INSTALL_DIR}/lib)
 
 #spdlog 라이브러리 다운로드 및 빌드
 ExternalProject_Add(
-    dep-spdlog # 대표 이름
+    dep_spdlog # 대표 이름
     GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
     GIT_TAG "v1.x"
     GIT_SHALLOW 1
@@ -19,5 +19,25 @@ ExternalProject_Add(
 )
 
 # Dependecy 리스트 및 라이브러리 파일 리스트 추가
-set(DEP_LIST ${DEP_LIST} dep-spdlog)
+set(DEP_LIST ${DEP_LIST} dep_spdlog)
 set(DEP_LIBS ${DEP_LIBS} spdlog)
+
+
+#glfw
+
+ExternalProject_Add(
+  dep_glfw
+  GIT_REPOSITORY "https://github.com/glfw/glfw.git"
+  GIT_TAG "3.4"
+  GIT_SHALLOW 1
+  UPDATE_COMMAND ""
+  PATCH_COMMAND ""
+  TEST_COMMAND ""
+  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+  -DGLFW_BUILD_DOCS=OFF # 문서
+  -DGLFW_BUILD_EXAMPLES=OFF # 예제
+  -DGLFW_BUILD_TESTS=OFF # 테스트
+)
+
+set(DEP_LIST ${DEP_LIST} dep_glfw)
+set(DEP_LIBS ${DEP_LIBS} glfw3)
