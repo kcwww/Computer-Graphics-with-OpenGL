3b2b7347e90a7d181f2954e2e9e7dbb9b4490074
kcwww <cwkim0321@gmail.com>
2024-10-04 21:54:55 +0900

    init: series10

--- series10/src/context.cpp
+++ series10/src/context.cpp
@@ -2,14 +2,16 @@
 #include "image.h"
 #include <imgui.h>
 
-ContextUPtr Context::Create() {
+ContextUPtr Context::Create()
+{
     auto context = ContextUPtr(new Context());
     if (!context->Init())
         return nullptr;
     return std::move(context);
 }
 
-void Context::ProcessInput(GLFWwindow* window) {
+void Context::ProcessInput(GLFWwindow *window)
+{
     if (!m_cameraControl)
         return;
     const float cameraSpeed = 0.05f;
@@ -22,7 +24,7 @@ void Context::ProcessInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         m_cameraPos += cameraSpeed * cameraRight;
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        m_cameraPos -= cameraSpeed * cameraRight;    
+        m_cameraPos -= cameraSpeed * cameraRight;
 
     auto cameraUp = glm::normalize(glm::cross(-m_cameraFront, cameraRight));
     if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
@@ -31,13 +33,15 @@ void Context::ProcessInput(GLFWwindow* window) {
         m_cameraPos -= cameraSpeed * cameraUp;
 }
 
-void Context::Reshape(int width, int height) {
+void Context::Reshape(int width, int height)
+{
     m_width = width;
     m_height = height;
     glViewport(0, 0, m_width, m_height);
 }
 
-void Context::MouseMove(double x, double y) {
+void Context::MouseMove(double x, double y)
+{
     if (!m_cameraControl)
         return;
     auto pos = glm::vec2((float)x, (float)y);
@@ -47,30 +51,41 @@ void Context::MouseMove(double x, double y) {
     m_cameraYaw -= deltaPos.x * cameraRotSpeed;
     m_cameraPitch -= deltaPos.y * cameraRotSpeed;
 
-    if (m_cameraYaw < 0.0f)   m_cameraYaw += 360.0f;
-    if (m_cameraYaw > 360.0f) m_cameraYaw -= 360.0f;
+    if (m_cameraYaw < 0.0f)
+        m_cameraYaw += 360.0f;
+    if (m_cameraYaw > 360.0f)
+        m_cameraYaw -= 360.0f;
 
-    if (m_cameraPitch > 89.0f)  m_cameraPitch = 89.0f;
-    if (m_cameraPitch < -89.0f) m_cameraPitch = -89.0f;
+    if (m_cameraPitch > 89.0f)
+        m_cameraPitch = 89.0f;
+    if (m_cameraPitch < -89.0f)
+        m_cameraPitch = -89.0f;
 
-    m_prevMousePos = pos;    
+    m_prevMousePos = pos;
 }
 
-void Context::MouseButton(int button, int action, double x, double y) {
-    if (button == GLFW_MOUSE_BUTTON_RIGHT) {
-        if (action == GLFW_PRESS) {
+void Context::MouseButton(int button, int action, double x, double y)
+{
+    if (button == GLFW_MOUSE_BUTTON_RIGHT)
+    {
+        if (action == GLFW_PRESS)
+        {
             m_prevMousePos = glm::vec2((float)x, (float)y);
             m_cameraControl = true;
         }
-        else if (action == GLFW_RELEASE) {
+        else if (action == GLFW_RELEASE)
+        {
             m_cameraControl = false;
         }
     }
 }
 
-void Context::Render() {
-    if (ImGui::Begin("ui window")) {
-        if (ImGui::CollapsingHeader("light", ImGuiTreeNodeFlags_DefaultOpen)) {
+void Context::Render()
+{
+    if (ImGui::Begin("ui window"))
+    {
+        if (ImGui::CollapsingHeader("light", ImGuiTreeNodeFlags_DefaultOpen))
+        {
             ImGui::DragFloat3("l.position", glm::value_ptr(m_light.position), 0.01f);
             ImGui::DragFloat3("l.direction", glm::value_ptr(m_light.direction), 0.01f);
             ImGui::DragFloat2("l.cutoff", glm::value_ptr(m_light.cutoff), 0.1f, 0.0f, 180.0f);
@@ -81,13 +96,15 @@ void Context::Render() {
             ImGui::Checkbox("flash light mode", &m_flashLightMode);
         }
 
-        if (ImGui::CollapsingHeader("material", ImGuiTreeNodeFlags_DefaultOpen)) {
+        if (ImGui::CollapsingHeader("material", ImGuiTreeNodeFlags_DefaultOpen))
+        {
             ImGui::DragFloat("m.shininess", &m_material->shininess, 1.0f, 1.0f, 256.0f);
         }
 
         ImGui::Checkbox("animation", &m_animation);
 
-        if (ImGui::ColorEdit4("clear color", glm::value_ptr(m_clearColor))) {
+        if (ImGui::ColorEdit4("clear color", glm::value_ptr(m_clearColor)))
+        {
             glClearColor(m_clearColor.r, m_clearColor.g, m_clearColor.b, m_clearColor.a);
         }
         ImGui::Separator();
@@ -95,7 +112,8 @@ void Context::Render() {
         ImGui::DragFloat("camera yaw", &m_cameraYaw, 0.5f);
         ImGui::DragFloat("camera pitch", &m_cameraPitch, 0.5f, -89.0f, 89.0f);
         ImGui::Separator();
-        if (ImGui::Button("reset camera")) {
+        if (ImGui::Button("reset camera"))
+        {
             m_cameraYaw = 0.0f;
             m_cameraPitch = 0.0f;
             m_cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
@@ -107,29 +125,30 @@ void Context::Render() {
 
     m_cameraFront =
         glm::rotate(glm::mat4(1.0f),
-            glm::radians(m_cameraYaw), glm::vec3(0.0f, 1.0f, 0.0f)) *
+                    glm::radians(m_cameraYaw), glm::vec3(0.0f, 1.0f, 0.0f)) *
         glm::rotate(glm::mat4(1.0f),
-            glm::radians(m_cameraPitch), glm::vec3(1.0f, 0.0f, 0.0f)) *
+                    glm::radians(m_cameraPitch), glm::vec3(1.0f, 0.0f, 0.0f)) *
         glm::vec4(0.0f, 0.0f, -1.0f, 0.0f);
 
     auto projection = glm::perspective(glm::radians(45.0f),
-        (float)m_width / (float)m_height, 0.01f, 20.0f);
+                                       (float)m_width / (float)m_height, 0.01f, 20.0f);
     auto view = glm::lookAt(
         m_cameraPos,
         m_cameraPos + m_cameraFront,
         m_cameraUp);
 
-    
-
     glm::vec3 lightPos = m_light.position;
     glm::vec3 lightDir = m_light.direction;
-    if (m_flashLightMode) {
+    if (m_flashLightMode)
+    {
         lightPos = m_cameraPos;
         lightDir = m_cameraFront;
-    } else {
+    }
+    else
+    {
         auto lightModelTransform =
-        glm::translate(glm::mat4(1.0), m_light.position) *
-        glm::scale(glm::mat4(1.0), glm::vec3(0.1f));
+            glm::translate(glm::mat4(1.0), m_light.position) *
+            glm::scale(glm::mat4(1.0), glm::vec3(0.1f));
         m_simpleProgram->Use();
         m_simpleProgram->SetUniform("color", glm::vec4(m_light.ambient + m_light.diffuse, 1.0f));
         m_simpleProgram->SetUniform("transform", projection * view * lightModelTransform);
@@ -141,21 +160,16 @@ void Context::Render() {
     m_program->SetUniform("light.position", lightPos);
     m_program->SetUniform("light.direction", lightDir);
     m_program->SetUniform("light.cutoff", glm::vec2(
-        cosf(glm::radians(m_light.cutoff[0])),
-        cosf(glm::radians(m_light.cutoff[0] + m_light.cutoff[1]))));
+                                              cosf(glm::radians(m_light.cutoff[0])),
+                                              cosf(glm::radians(m_light.cutoff[0] + m_light.cutoff[1]))));
     m_program->SetUniform("light.attenuation", GetAttenuationCoeff(m_light.distance));
     m_program->SetUniform("light.ambient", m_light.ambient);
     m_program->SetUniform("light.diffuse", m_light.diffuse);
     m_program->SetUniform("light.specular", m_light.specular);
-
-    auto modelTransform = glm::mat4(1.0f);
-    auto transform = projection * view * modelTransform;
-    m_program->SetUniform("transform", transform);
-    m_program->SetUniform("modelTransform", modelTransform);
-    m_model->Draw(m_program.get());
 }
 
-bool Context::Init() {
+bool Context::Init()
+{
     glEnable(GL_DEPTH_TEST);
     glClearColor(m_clearColor.r, m_clearColor.g, m_clearColor.b, m_clearColor.a);
 
@@ -169,20 +183,16 @@ bool Context::Init() {
     if (!m_program)
         return false;
 
-
-
-    m_model = Model::Load("./model/backpack.obj");
-    if (!m_model)
-        return false;
-
     m_material = Material::Create();
     m_material->diffuse = Texture::CreateFromImage(
         Image::CreateSingleColorImage(4, 4,
-            glm::vec4(1.0f, 1.0f, 1.0f, 1.0f)).get());
+                                      glm::vec4(1.0f, 1.0f, 1.0f, 1.0f))
+            .get());
 
     m_material->specular = Texture::CreateFromImage(
         Image::CreateSingleColorImage(4, 4,
-            glm::vec4(0.5f, 0.5f, 0.5f, 1.0f)).get());
+                                      glm::vec4(0.5f, 0.5f, 0.5f, 1.0f))
+            .get());
 
     return true;
 }
