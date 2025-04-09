cdd607d6a919b8939113f9a10bfb8b7d43742c5f
kcwww <cwkim0321@gmail.com>
2025-04-09 23:23:18 +0900

    chore: add comment

--- series12/shader/lighting_shadow.fs
+++ series12/shader/lighting_shadow.fs
@@ -46,6 +46,9 @@ float ShadowCalculation(vec4 fragPosLight, vec3 normal, vec3 lightDir) {
     // check whether current frag pos is in shadow
     float bias = max(0.01 * (1.0 - dot(normal, lightDir)), 0.001);
     float shadow = 0.0;
+
+    // PCF filtering
+    // we sample the depth map at 9 different positions around the current fragment
     vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
     for(int x = -1; x <= 1; ++x) {
         for (int y = -1; y <= 1; ++y) {
