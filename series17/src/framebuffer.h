53e1bc9ff1fdb629a134c79802cd2449a349be84
kcwww <cwkim0321@gmail.com>
2025-05-03 22:31:17 +0900

    feat: add cubemap framebuffer

--- series17/src/framebuffer.h
+++ series17/src/framebuffer.h
@@ -25,4 +25,27 @@ private:
   std::vector<TexturePtr> m_colorAttachments;
 };
 
+void CubeTexture::Init(int width, int height, int format, uint32_t type)
+{
+  glGenTextures(1, &m_texture);
+  Bind();
+
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+  m_width = width;
+  m_height = height;
+  m_type = type;
+  m_format = format;
+  GLenum imageFormat = GetImageFormat(m_format);
+
+  for (uint32_t i = 0; i < 6; i++)
+  {
+    glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, m_format, m_width, m_height, 0, imageFormat, m_type, nullptr);
+  }
+}
+
 #endif // __FRAMEBUFFER_H__
